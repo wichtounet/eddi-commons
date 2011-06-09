606e82d994e179d3fff30b4a15e7b044a27a0654
Baptiste Wicht <baptistewicht@gmail.com>
2011-06-08 20:33:05 -0700

    Change the way strings are written on bytecode Solve bug in bytecode write

--- src/ByteCode.cpp
+++ src/ByteCode.cpp
@@ -21,14 +21,21 @@ class OneOperandCall {
 
 template<typename T>
 std::ostream& binary_write(std::ostream* os, const T& value){
-    return os->write(reinterpret_cast<const char*>(&value), sizeof(T));
+	return os->write(reinterpret_cast<const char*>(&value), sizeof(T));
+}
+
+void writeString(ofstream* outStream, string litteral){
+	binary_write(outStream, "S");
+	binary_write(outStream, litteral.length());
+	binary_write(outStream, litteral);
 }
 
 void writeOneOperandCall(ofstream* outStream, ByteCode bytecode, string litteral){
-	binary_write(outStream, &bytecode);
-	binary_write(outStream, &litteral);
+	binary_write(outStream, (int) bytecode);
+	
+	writeString(outStream, litteral);
 }
 
 void writeSimpleCall(ofstream* outStream, ByteCode bytecode){
-	binary_write(outStream, &bytecode);
+	binary_write(outStream, (int) bytecode);
 }
