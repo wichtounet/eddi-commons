5473bb04135236dd82b9f77945a57b76b473d0f9
Baptiste Wicht <baptistewicht@gmail.com>
2011-06-08 22:12:51 -0700

    Improve the utilities to read bytecode

--- src/ByteCode.cpp
+++ src/ByteCode.cpp
@@ -19,23 +19,49 @@ class OneOperandCall {
 	OneOperandCall(ByteCode bytecode, string litteral) : m_bytecode(bytecode), m_litteral(litteral) {}
 };
 
-template<typename T>
-std::ostream& binary_write(std::ostream* os, const T& value){
-	return os->write(reinterpret_cast<const char*>(&value), sizeof(T));
+/* Write */
+
+std::ostream& binary_write_string(std::ostream* stream, string value){
+	return stream->write(value.c_str(), value.length());
 }
 
-void writeString(ofstream* outStream, string litteral){
-	binary_write(outStream, "S");
-	binary_write(outStream, litteral.length());
-	binary_write(outStream, litteral);
+void writeLitteral(ofstream* stream, string litteral){
+	binary_write(stream, 'S');
+	binary_write(stream, litteral.length() - 2);
+	binary_write_string(stream, litteral.substr(1, litteral.length() - 2));
 }
 
-void writeOneOperandCall(ofstream* outStream, ByteCode bytecode, string litteral){
-	binary_write(outStream, (int) bytecode);
+void writeOneOperandCall(ofstream* stream, ByteCode bytecode, string litteral){
+	binary_write(stream, (int) bytecode);
 	
-	writeString(outStream, litteral);
+	writeLitteral(stream, litteral);
 }
 
-void writeSimpleCall(ofstream* outStream, ByteCode bytecode){
-	binary_write(outStream, (int) bytecode);
+void writeSimpleCall(ofstream* stream, ByteCode bytecode){
+	binary_write(stream, (int) bytecode);
+}
+
+void writeEnd(ofstream* stream){
+	binary_write(stream, (int) END);
+}
+
+/* Read */
+
+ByteCode readByteCode(ifstream* stream){
+	int bytecode;
+	binary_read(stream, bytecode);
+	return (ByteCode) bytecode;
+}
+
+std::string binary_read_string(std::istream* stream){
+	size_t size;
+	binary_read(stream, size);
+	
+	char* buffer = new char[size + 1];
+	stream->read(buffer, size);
+	buffer[size] = 0;
+	string litteral = buffer;
+	delete[] buffer;	
+
+	return litteral;
 }
